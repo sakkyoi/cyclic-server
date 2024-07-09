e23a89d4375241f4b64c656e11453b4c8a5ce0b7
sakkyoi <22865542+sakkyoi@users.noreply.github.com>
2024-07-09 14:55:08 +0800

    update user get self data

--- router/api/user/get.go
+++ router/api/user/get.go
@@ -1,12 +1,35 @@
 package user
 
 import (
+	"cyclic/ent"
+	"cyclic/ent/user"
 	"cyclic/pkg/magistrate"
+	"cyclic/pkg/secretary"
+	"cyclic/router/model"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"net/http"
 )
 
-func (u *User) Get(c *gin.Context) {
-	claims := c.MustGet("claims").(*magistrate.Claims)
-	c.JSON(http.StatusOK, gin.H{"message": "get user", "claims": claims})
+func (*User) Get(c *gin.Context) {
+	// parse id from claims into uuid
+	id, err := uuid.Parse(c.MustGet("claims").(*magistrate.Claims).Subject)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, model.ErrorResponse{Type: model.ErrorUnauthorized, Error: "invalid token", Detail: err.Error()})
+		return
+	}
+
+	// get the user
+	u, err := secretary.Minute.User.Query().
+		Where(user.ID(id)).
+		Only(c)
+	if ent.IsNotFound(err) {
+		c.AbortWithStatusJSON(http.StatusNotFound, model.ErrorResponse{Type: model.ErrorUserNotFound, Error: "user not found"})
+		return
+	} else if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, model.ErrorResponse{Type: model.ErrorInternal, Error: "internal error", Detail: err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, model.Response{Data: u})
 }
